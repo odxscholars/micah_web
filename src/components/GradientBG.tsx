89ee9b456b8658ab38a6c769cd32a8f030ed110a
odxscholars <shemgithub@protonmail.com>
2025-09-08 20:11:44 +0800

    changed BG

--- src/components/GradientBG.tsx
+++ src/components/GradientBG.tsx
@@ -1,16 +1,27 @@
-import { motion, useReducedMotion } from 'framer-motion';
-import { ReactNode } from 'react';
+import {motion, useReducedMotion} from 'framer-motion';
+import {ReactNode} from 'react';
 import styles from './GradientBG.module.css';
 
-export default function GradientBG({ children }: { children: ReactNode }) {
-  const reduce = useReducedMotion();
-  return (
-    <motion.div
-      className={styles.bg}
-      animate={reduce ? undefined : { backgroundPosition: ['0% 0%', '100% 100%'] }}
-      transition={reduce ? undefined : { duration: 20, repeat: Infinity, repeatType: 'reverse' }}
-    >
-      {children}
-    </motion.div>
-  );
+export default function GradientBG({children}: { children: ReactNode }) {
+    const reduce = useReducedMotion();
+    return (
+        <motion.div
+            className={styles.bg}
+            animate={
+                reduce
+                    ? undefined
+                    : {
+                        backgroundPosition: ['0% 50%', '100% 50%'],
+                    }
+            }
+            transition={
+                reduce
+                    ? undefined
+                    : {duration: 2, repeat: Infinity, repeatType: 'loop', ease: 'linear'}
+            }
+        >
+            {children}
+        </motion.div>
+
+    );
 }
